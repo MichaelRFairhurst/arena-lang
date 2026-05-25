bc31f0ed621ec1635b238f7568d8f8d73fc27b36
Mike Fairhurst <michaelrfairhurst@github.com>
2026-05-25 09:42:15 -0700

    Reorganize project for separate backends, runtime, etc

--- compile/llvm/src/main.cpp
+++ compile/llvm/src/main.cpp
@@ -3,41 +3,6 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Verifier.h"
 #include "llvm/Support/raw_ostream.h"
-#include "parse/parse.hpp"
-#include <iostream>
-#include <fstream>
-#include <filesystem>
-
-int main(int argc, char **argv) {
-    /// auto ast = arena::parse::parse("a.*.*.b.& * 3.as(int)");
-    if (argc == 1) {
-        std::cerr << "Usage: " << argv[0] << " <source-file>\n";
-        return 1;
-    } else if (argc > 2) {
-        std::cerr << "Error: Only one file at a time is currently supported.\n";
-        return 1;
-    }
-
-    // create a filesystem path
-    std::filesystem::path file(argv[1]);
-    if (!std::filesystem::exists(file)) {
-        std::cerr << "Error: File does not exist: " << file << "\n";
-        return 1;
-    }
-
-    // read the file into a string
-    std::ifstream input_file(file);
-    if (!input_file) {
-        std::cerr << "Error: Could not open file: " << file << "\n";
-        return 1;
-    }
-
-    // TODO: support larger files
-    std::string input((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
-
-    auto ast = arena::parse::parse(input);
-    std::cout << "Parsed AST: " << ast->to_string() << std::endl;
-}
 
 int main_llvm() {
     // Create an LLVM context and module
